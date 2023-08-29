dd9e385c10440b76f4cd53f3e5a32e872af11b8e
Maicon Santana <87348009+stn8m0n@users.noreply.github.com>
2023-08-29 05:07:35 -0300

    Manual addc.
    Adcionando manualmente os arquivos

---
+++ main.js
@@ -0,0 +1,20 @@
+
+const form = document.getElementById('form-valida');
+const campoA = document.getElementById('campo-a');
+const campoB = document.getElementById('campo-b');
+const mensagemSucesso= document.querySelector('.sucess-message');
+const mensagemErro= document.querySelector('.error-message');
+
+form.addEventListener('submit', function(){
+    Event.preventDefault();
+
+    const valorcampoA = parseFloat(campoA.value);
+    const valorcampoB = parseFloat(campoB.value);
+    
+    if (valorcampoA > valorcampoB){
+
+        mensagem.textContent = mensagemSucesso 
+    } else {
+        mensagem.textContent = mensagemErro
+    }
+})
